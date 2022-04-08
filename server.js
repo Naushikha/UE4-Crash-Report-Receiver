ef1d8d8282216c461b591c0846ce5d7e18a9ce78
Naushikha Jayawickrama <naushikha.jayawickrama@gmail.com>
2022-04-08 23:19:51 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const fs = require("fs");
+const crypto = require("crypto");
+var serveIndex = require('serve-index');
+const { extractCrashReports } = require("./ue4CrashExtractor");
+
+const PORT = 8080;
+const UPLOAD_DIR = "./uploads"; // TODO: Add environment var for dir, port
+const REPORTS_DIR = "./reports";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (!fs.existsSync(UPLOAD_DIR)) fs.mkdirSync(UPLOAD_DIR);
+
+app.get("/", (req, res) =>
+{
+  res.status(200).send("CRR ready.");
+});
+
+app.post("/upload", (req, res) => 
+{
+  var uniqueDTID = getDateTimeString();
+
+  req.pipe(fs.createWriteStream(`${UPLOAD_DIR}/${uniqueDTID}.ue4crash`));
+  fs.writeFileSync(`${UPLOAD_DIR}/${uniqueDTID}.json`, JSON.stringify(req.query, null, "\t"));
+  console.log("(✔) Received a crash file.");
+
+  res.status(201).send("Report uploaded.");
+
+  // Schedule extraction
+  setTimeout(() => 
+  {
+    extractCrashReports();
+  }, 3000);
+});
+
+app.listen(PORT, () =>
+{
+  console.log("(+) Crash Report Receiver started.");
+  extractCrashReports(); // Run extraction now
+});
+
+setInterval(() => 
+{
+  extractCrashReports(); // Periodical extraction
+}, 1000 * 60 * 10); // Every 10 mins
+
+// Serve extracted reports as an index for now
+app.use("/reports", serveIndex(REPORTS_DIR));
+app.use("/reports", express.static(REPORTS_DIR));
+
+const getDateTimeString = () =>
+{
+  // https://stackoverflow.com/a/30272803
+  // e.g. 2022_08_29_10_09_32
+  var d = new Date();
+  var dateTime = 
+    d.getFullYear() + "_" + 
+    ("0"+ (d.getMonth() + 1 )).slice(-2) + "_" +
+    ("0" + d.getDate()).slice(-2) + "_" + 
+    ("0" + d.getHours()).slice(-2) + "_" + ("0" + d.getMinutes()).slice(-2) + "_" + crypto.randomBytes(20).toString("hex").slice(-5);
+
+    return dateTime;
+}
---
+++ ue4CrashExtractor.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const path = require("path");
+const zlib = require("zlib");
+
+const UPLOAD_DIR = "./uploads";
+const REPORTS_DIR = "./reports";
+
+exports.extractCrashReports = async () =>
+{
+  console.log(`(!) Starting crash report extraction...`);
+  fs.readdir(UPLOAD_DIR, (err, crashFiles) => {
+    if (!fs.existsSync(REPORTS_DIR)) fs.mkdirSync(REPORTS_DIR);
+  
+    crashFiles.forEach(crashFile => {
+      var crashFileName = path.parse(crashFile).name;
+      var crashFileExt = path.parse(crashFile).ext;
+      if (crashFileExt != ".ue4crash") return;
+      
+      var reportDir = `${REPORTS_DIR}/${crashFileName}`;
+      if (!fs.existsSync(reportDir)) fs.mkdirSync(reportDir);
+      
+      console.log(`(*) Working on file '${crashFile}'...`);
+  
+      var fileBuffer = fs.readFileSync(`${UPLOAD_DIR}/${crashFile}`);
+      zlib.inflate(fileBuffer, (err, buf) => {
+        // HEADER
+        // https://stackoverflow.com/a/16202908
+        // 0 - 3 <-- 4 bytes 04 01 00 00 | IS THIS REALLY A MAGIC NUMBER?, always infront of a string
+        var DirectoryName = buf.slice(4, 264).toString("utf8").replace(/\0.*$/g,''); // 3 - 262 <-- FString: 260 bytes
+        // 264 - 267 <-- 4 bytes 04 01 00 00
+        var FileName = buf.slice(268, 528).toString("utf8");  // 268 - 528 <-- FString: 260 bytes
+        var UncompressedSize = buf.readInt32LE(528); // 528 - 531 <-- int32: 4 bytes 
+        var FileCount = buf.readInt32LE(532); // 532 - 535 <-- int32: 4 bytes 
+        // console.log(DirectoryName);console.log(FileName);console.log(UncompressedSize);console.log(FileCount);
+        
+        // READING ALL FILES
+        var bytePtr = 536;
+        var bufEnd = buf.length - 1;
+        while (bytePtr < bufEnd)
+        {
+          // FILE
+          var CurrentFileIndex = buf.readInt32LE(bytePtr); // 536 - 539 <-- int32: 4 bytes 
+          // 540 - 543 <-- 4 bytes 04 01 00 00
+          bytePtr += 8;
+          var FileName = buf.slice(bytePtr, bytePtr + 260).toString("utf8").replace(/\0.*$/g,'');  // 544 - 803 <-- FString: 260 bytes
+          bytePtr += 260;
+          var FileSize = buf.readInt32LE(bytePtr); // 804 - 807 <-- int32: 4 bytes 
+          bytePtr += 4;
+          var FileData = buf.slice(bytePtr, bytePtr + FileSize); // 808 -->
+          bytePtr += FileSize;
+  
+          fs.writeFileSync(`${reportDir}/${FileName}`, FileData);
+  
+        }
+        fs.rename(`${UPLOAD_DIR}/${crashFileName}.ue4crash`, `${reportDir}/${DirectoryName}.ue4crash`, () => {});
+        fs.rename(`${UPLOAD_DIR}/${crashFileName}.json`, `${reportDir}/${DirectoryName}.json`, () => {});
+        // console.log(CurrentFileIndex, FileName, FileSize);
+      });
+
+      console.log(`(✔) File '${crashFile}' extracted.`);
+    });
+    console.log(`(✔) Extraction complete.`);
+  });
+}
+
+
+// REFERENCES
+// CrashUpload.cpp - https://github.com/EpicGames/UnrealEngine/blob/4.27/Engine/Source/Runtime/CrashReportCore/Private/CrashUpload.cpp
+// https://github.com/EpicGames/UnrealEngine/blob/4.17/Engine/Source/Programs/CrashReporter/CrashReportProcess/DataRouterReportQueue.cs
