e4d7bb61d687cb06de38cefee7f4e0a25e52d7e5
Naushikha Jayawickrama <naushikha.jayawickrama@gmail.com>
2022-04-09 14:09:05 +0530

    Updated to utilize the power of async!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const crypto = require("crypto");
-var serveIndex = require('serve-index');
+var serveIndex = require("serve-index");
 const { extractCrashReports } = require("./ue4CrashExtractor");
 
 const PORT = process.env.PORT || 8080;
@@ -13,54 +13,69 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (!fs.existsSync(UPLOAD_DIR)) fs.mkdirSync(UPLOAD_DIR);
+if (!fs.existsSync(REPORTS_DIR)) fs.mkdirSync(REPORTS_DIR);
 
-app.get("/", (req, res) =>
-{
+app.get("/", (req, res) => {
   res.status(200).send("CRR ready.");
 });
 
-app.post("/upload", (req, res) => 
-{
+var immediateSched = false; // Makes sure immediate scheduling doesn't get repetitive for quick consecutive requests
+
+app.post("/upload", (req, res) => {
   var uniqueDTID = getDateTimeString();
-
   req.pipe(fs.createWriteStream(`${UPLOAD_DIR}/${uniqueDTID}.ue4crash`));
-  fs.writeFileSync(`${UPLOAD_DIR}/${uniqueDTID}.json`, JSON.stringify(req.query, null, "\t"));
-  console.log("(✔) Received a crash file.");
+  fs.writeFile(
+    `${UPLOAD_DIR}/${uniqueDTID}.json`,
+    JSON.stringify(req.query, null, "\t"),
+    (err) => {
+      if (err) {
+        console.log("(✖) Receiving crash file failed.", "\n", err);
+        res.status(409).send("Report uploading failed."); // https://stackoverflow.com/a/7088468
+      } else {
+        console.log("(✔) Received a crash file.");
+        res.status(201).send("Report uploaded.");
 
-  res.status(201).send("Report uploaded.");
-
-  // Schedule extraction
-  setTimeout(() => 
-  {
-    extractCrashReports();
-  }, 3000);
+        if (!immediateSched) {
+          setTimeout(() => {
+            extractCrashReports(); // Schedule extraction
+            immediateSched = false;
+          }, 3000);
+          immediateSched = true;
+        }
+      }
+    }
+  );
 });
 
-app.listen(PORT, () =>
-{
+app.listen(PORT, () => {
   console.log("(+) Crash Report Receiver started.");
   extractCrashReports(); // Run extraction now
 });
-
-setInterval(() => 
-{
-  extractCrashReports(); // Periodical extraction
-}, 1000 * 60 * 10); // Every 10 mins
 
 // Serve extracted reports as an index for now
 app.use("/reports", serveIndex(REPORTS_DIR));
 app.use("/reports", express.static(REPORTS_DIR));
 
-const getDateTimeString = () =>
-{
+setInterval(() => {
+  extractCrashReports(); // Periodical extraction
+}, 1000 * 60 * 10); // Every 10 mins
+
+const getDateTimeString = () => {
   // https://stackoverflow.com/a/30272803
   // e.g. 2022_08_29_10_09_32
   var d = new Date();
-  var dateTime = 
-    d.getFullYear() + "_" + 
-    ("0"+ (d.getMonth() + 1 )).slice(-2) + "_" +
-    ("0" + d.getDate()).slice(-2) + "_" + 
-    ("0" + d.getHours()).slice(-2) + "_" + ("0" + d.getMinutes()).slice(-2) + "_" + crypto.randomBytes(20).toString("hex").slice(-5);
+  var dateTime =
+    d.getFullYear() +
+    "_" +
+    ("0" + (d.getMonth() + 1)).slice(-2) +
+    "_" +
+    ("0" + d.getDate()).slice(-2) +
+    "_" +
+    ("0" + d.getHours()).slice(-2) +
+    "_" +
+    ("0" + d.getMinutes()).slice(-2) +
+    "_" +
+    crypto.randomBytes(20).toString("hex").slice(-5);
 
-    return dateTime;
-}
+  return dateTime;
+};
